36771c7e4d1f32887847957d6b3d5024eff1af03
Siaoja <1610692147@qq.com>
2020-07-22 16:20:37 +0800

    [JavaOO-parctice06][spike lu]feature:complete practice06 and through test

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,26 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private int klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+
+    public int getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce(){
+        return super.introduce() + " I am a Teacher. I teach " + ((this.klass!=0)?"Class "+ this.klass + ".":"No Class.");
+    }
+
+
 }
