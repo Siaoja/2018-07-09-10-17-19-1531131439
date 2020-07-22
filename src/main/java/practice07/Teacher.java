db91085c44d91d9eab7e3661075e19c41dc5b692
Siaoja <1610692147@qq.com>
2020-07-22 16:39:39 +0800

    [JavaOO-parctice07][spike lu]feature:complete practice07 and through test

--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,28 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce(){
+        return super.introduce() + " I am a Teacher. I teach " + ((this.klass!=null)? this.klass.getDisplayName() + ".":"No Class.");
+    }
+
+    public String introduceWith(Student student){
+        String relationWithStudent = ((student.getKlass().getNumber() == this.klass.getNumber()) ? "I teach ":"I don't teach ") + student.getName() + ".";
+        return  super.introduce() + " I am a Teacher. " + relationWithStudent;
+    }
 }
