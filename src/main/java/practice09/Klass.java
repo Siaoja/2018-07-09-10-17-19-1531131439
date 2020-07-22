4cfa37fa2606da598f3fbb46c80411da8a4079e5
Siaoja <1610692147@qq.com>
2020-07-22 19:59:04 +0800

    [JavaOO-practices09][spike lu]feature:complete practices09 and through the test

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,35 @@
 package practice09;
 
+
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public String getDisplayName(){
+        return "Class " + number;
+    }
+
+    public void assignLeader(Student student){
+        if(this.number == student.getKlass().getNumber()){
+            leader = student;
+        }else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public void appendMember(Student student){
+        student.setKlass(this);
+    }
 }
--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,29 @@
 package practice09;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce(){
+        String relationWithClass = " I teach " + ((this.klass!=null)? this.klass.getDisplayName() + ".":"No Class.");
+        return super.introduce() + " I am a Teacher." + relationWithClass;
+    }
+
+    public String introduceWith(Student student){
+        String relationWithStudent = ((student.getKlass().getNumber() == this.klass.getNumber()) ? "I teach ":"I don't teach ") + student.getName() + ".";
+        return  super.introduce() + " I am a Teacher. " + relationWithStudent;
+    }
 }
