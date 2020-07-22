194baa90c2c9beb973c1137d67df1b0c465bcefa
Siaoja <1610692147@qq.com>
2020-07-22 17:41:44 +0800

    [JavaOO-parctice08][spike lu]feature:complete practice08 three steps and through test

--- src/main/java/practice08/Student.java
+++ src/main/java/practice08/Student.java
@@ -1,4 +1,21 @@
 package practice08;
 
-public class Student {
+public class Student extends Person{
+    private Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce(){
+        String introduce = ((klass.getLeader().getId() == this.getId()) ? "I am Leader of Class 2." : "I am at Class 2");
+        return super.introduce() + " I am a Student. " + introduce;
+    }
+
 }
